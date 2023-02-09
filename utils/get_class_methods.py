a2eaccff10b860b22f853d5cc09d8a8688535566
Allyson Robert <allyson.robert@outlook.com>
2023-02-09 17:49:27 +0100

    Dev v2: Classes can also have private methods that need to be excluded from the search

--- utils/get_class_methods.py
+++ utils/get_class_methods.py
@@ -2,8 +2,8 @@ def get_class_methods(cls):
     methods = []
     # Get all class attributes
     for attribute in dir(cls):
-        # Add callable attributes that aren't dunders
-        if callable(getattr(cls, attribute)) and not attribute.startswith('__'):
+        # Add callable attributes that aren't dunders or private methods
+        if callable(getattr(cls, attribute)) and not attribute.startswith('_'):
             # Ignore setters
             if not attribute.startswith('set'):
                 methods.append(attribute)
