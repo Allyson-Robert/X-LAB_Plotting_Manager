92f82f8592374652c0767291eb6cd5368fa6d3d9
Allyson Robert <allyson.robert@outlook.com>
2023-03-10 11:24:09 +0100

    Dev v2: Added DataCore that implements get_data get_units and get_allowed_observables by default

--- data/datatypes/data.py
+++ data/datatypes/data.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 
+# Abstract class for all data types
 class Data(ABC):
 
     @abstractmethod
@@ -18,3 +19,27 @@ class Data(ABC):
     @abstractmethod
     def get_allowed_observables(self):
         pass
+
+
+# Master class with implementation of 1) get_data, 2) get_units and 3) get_allowed_observables
+class DataCore(Data):
+    def __init__(self):
+        self.raw_data = {}
+        self._allowed_observables = {}
+
+    @abstractmethod
+    def read_file(self, filepath: str) -> None:
+        pass
+
+    def get_data(self, observable: str):
+        if observable in self._allowed_observables:
+            return self.raw_data[observable]['data']
+        else:
+            raise ValueError(f"PDSData does not contain {observable} data")
+
+    def get_units(self, observable: str) -> str:
+        self.get_data(observable)
+        return self.raw_data[observable]["units"]
+
+    def get_allowed_observables(self):
+        return self._allowed_observables
--- data/datatypes/scatter_data/iv_scatter.py
+++ data/datatypes/scatter_data/iv_scatter.py
@@ -1,11 +1,11 @@
-from data.datatypes.data import Data
+from data.datatypes.data import DataCore
 from utils.file_readers.read_csv import read_csv
 from datetime import datetime
 import re
 import os
 
 
-class IVData(Data):
+class IVData(DataCore):
     def __init__(self, label):
         self.raw_data = {
             "label": {"units": "N/A", "data": label},
@@ -17,18 +17,18 @@ class IVData(Data):
         self.label_format = "%Y_%m_%d_%H_%M_%S"
         self.dt_pattern = '\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}'
 
-    def get_data(self, observable: str):
-        if observable in self._allowed_observables:
-            return self.raw_data[observable]['data']
-        else:
-            raise ValueError(f"IVData does not contain {observable} data")
-
-    def get_units(self, observable: str) -> str:
-        self.get_data(observable)
-        return self.raw_data[observable]["units"]
-
-    def get_allowed_observables(self):
-        return self._allowed_observables
+    # def get_data(self, observable: str):
+    #     if observable in self._allowed_observables:
+    #         return self.raw_data[observable]['data']
+    #     else:
+    #         raise ValueError(f"IVData does not contain {observable} data")
+    #
+    # def get_units(self, observable: str) -> str:
+    #     self.get_data(observable)
+    #     return self.raw_data[observable]["units"]
+    #
+    # def get_allowed_observables(self):
+    #     return self._allowed_observables
 
     def read_file(self, filepath: str):
         data = read_csv(filepath)
