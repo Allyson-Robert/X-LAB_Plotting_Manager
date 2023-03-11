34046c2f405746a4cbf6ee89d2d69b370490b496
Allyson Robert <allyson.robert@outlook.com>
2023-03-11 19:14:53 +0100

    Dev v2: Added all classes to __init__

---
+++ plotter/__init__.py
@@ -0,0 +1,7 @@
+from .plotter import Plotter
+from .histogram_plotter import HistogramPlotter
+from .heatmap_plotter import HeatmapPlotter
+from .scatter_data_plotter import ScatterDataPlotter
+from .iv_stability_plotter import IVStabilityPlotter
+
+__all__ = ["Plotter", "HistogramPlotter", "HeatmapPlotter", "ScatterDataPlotter", "IVStabilityPlotter"]
