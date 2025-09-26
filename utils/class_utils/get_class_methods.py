e353d4bf268436aef8f00076e9b21d0373887c5e
Allyson-Robert <40757141+Allyson-Robert@users.noreply.github.com>
2025-09-26 10:02:33 +0200

    get_class_methods should return methods in order of definition. inspect.getmembers() sorts by name instead

--- utils/class_utils/get_class_methods.py
+++ utils/class_utils/get_class_methods.py
@@ -1,10 +1,15 @@
-import inspect
+from types import FunctionType
 
 
+# Returns class methods from class.__items__() that are functions
 def get_class_methods(cls, ignore=[]) -> list:
     methods = []
-    # Apparently there is a function to get the members from a class
-    for name, func in inspect.getmembers(cls, predicate=inspect.isfunction):
+    for name, func in cls.__dict__.items():
+        # Skip items that are not of the function type
+        if type(func) != FunctionType:
+            continue
+        # ignore dunder_methods, setters and explicitly ignored function names
         if not name.startswith('_') and not name.startswith('set') and name not in ignore:
             methods.append(name)
+
     return methods
