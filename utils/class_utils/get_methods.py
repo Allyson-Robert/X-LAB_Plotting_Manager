d5fa7268014c1466efcf3c8a2878c6648caeb35b
Allyson-Robert <40757141+Allyson-Robert@users.noreply.github.com>
2025-09-26 09:48:28 +0200

    get_methods to be replaced by get_class_methods

--- utils/class_utils/get_methods.py
+++ utils/class_utils/get_methods.py
@@ -1,5 +1,8 @@
+# Deprecating
 from types import FunctionType
+import warnings
 
 
 def get_methods(cls):
+    warnings.warn("Function will be removed in stable release", DeprecationWarning)
     return [x for x, y in cls.__dict__.items() if type(y) == FunctionType]
