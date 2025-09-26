d164e8532b316f593434f8fe8b9c4b184d02ac62
Allyson-Robert <40757141+Allyson-Robert@users.noreply.github.com>
2025-09-26 09:49:34 +0200

    fixed comment that got incorrectly refactored

--- utils/class_utils/get_class_methods.py
+++ utils/class_utils/get_class_methods.py
@@ -3,7 +3,7 @@ import inspect
 
 def get_class_methods(cls, ignore=[]) -> list:
     methods = []
-    # Apparently there is a function to get the members from a class_utils
+    # Apparently there is a function to get the members from a class
     for name, func in inspect.getmembers(cls, predicate=inspect.isfunction):
         if not name.startswith('_') and not name.startswith('set') and name not in ignore:
             methods.append(name)
