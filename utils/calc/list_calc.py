4f10c0514922981fcffdcc93affbea3f842fa1c3
Allyson-Robert <40757141+Allyson-Robert@users.noreply.github.com>
2025-11-19 09:39:24 +0100

    check list length, do not flatten empty list

--- utils/calc/list_calc.py
+++ utils/calc/list_calc.py
@@ -1,4 +1,6 @@
 def flatten_matrix(input: list[list]) -> list:
+    if len(input) < 1:
+        raise ValueError("Flatten Matrix Will not Flatten empty list")
     output = []
     for sublist in input:
         for item in sublist:
