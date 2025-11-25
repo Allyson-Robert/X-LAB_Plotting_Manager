251cb2e0ccf124786e89f51c05eed4e92ce85bc8
Allyson-Robert <40757141+Allyson-Robert@users.noreply.github.com>
2025-11-25 14:03:53 +0100

    class docstrings

--- dataspec_manager/dataspec_json_encoder.py
+++ dataspec_manager/dataspec_json_encoder.py
@@ -3,6 +3,20 @@ import datetime
 
 
 class DataSpecJSONEncoder(JSONEncoder):
+    """
+    JSON encoder for `DataSpec` objects and related dataclasses.
+
+    This encoder provides two custom behaviours:
+
+    - ``datetime.datetime`` instances are serialised to a compact string
+      representation using the format ``"%Y.%m.%d_%H.%M.%S"``. This matches
+      the format expected by `DataSpec` and the corresponding JSON decoder.
+    - All other objects are serialised via their ``__dict__`` attribute,
+      which is sufficient for simple container-like classes such as `DataSpec`.
+
+    The encoder is intended to be used together with `DataSpecJSONDecoder` to
+    provide a round-trip-safe JSON representation of dataspecs.
+    """
     def default(self, o):
         if isinstance(o, datetime.datetime):
             return o.strftime("%Y.%m.%d_%H.%M.%S")
--- dataspec_manager/dataspec_manager.py
+++ dataspec_manager/dataspec_manager.py
@@ -5,6 +5,23 @@ from dataspec_json_decoder import DataSpecJSONDecoder
 
 
 class DataSpecManager:
+    """
+       Small helper class for persisting `DataSpec` instances to and from JSON files.
+
+       Responsibilities
+       ----------------
+       - `save_dataspec(dataspec, file_name)`:
+           Serialises a `DataSpec` instance to disk using `DataSpecJSONEncoder`.
+           The method asserts that the passed object is a `DataSpec` and writes
+           the encoded JSON to the given file path.
+       - `open_dataspec(file_name)`:
+           Opens a JSON file and deserialises it into a `DataSpec` instance using
+           `DataSpecJSONDecoder`.
+
+       The manager does not interpret the dataspec content; it only handles the
+       IO and wiring between JSON encoder/decoder and the underlying `DataSpec`
+       objects.
+   """
     def __init__(self):
         pass
 
