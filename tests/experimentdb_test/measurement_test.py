1c881381a652a11ae3bd4ea22a9abc9eda49a680
Allyson-Robert <40757141+Allyson-Robert@users.noreply.github.com>
2025-11-26 09:41:36 +0100

    plotly colours more explicitly named and separated

--- tests/experimentdb_test/measurement_test.py
+++ tests/experimentdb_test/measurement_test.py
@@ -8,7 +8,7 @@ def test_measurement(filepath: str, label: str, colour: str):
     print(m.get_label())
     print(m.get_location())
     print(m.get_datetime())
-    print(m.get_colour())
+    print(m.get_plotly_colour())
 
 test_measurement(
     "G:/My Drive/Data/Sunbrick/2023/01-January/test/Position_1/IV_2022_12_20_09_29_06.txt",
