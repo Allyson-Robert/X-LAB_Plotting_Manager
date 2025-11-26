1cb1d40cdc01799b2b2a672256027e6732b76ccd
Allyson-Robert <40757141+Allyson-Robert@users.noreply.github.com>
2025-11-26 11:35:54 +0100

    strings rather than objects

--- dataspec_manager/__init__.py
+++ dataspec_manager/__init__.py
@@ -2,4 +2,4 @@ from .dataspec import DataSpec
 from .dataspec_json_encoder import DataSpecJSONEncoder
 from .dataspec_json_decoder import DataSpecJSONDecoder
 
-__all__ = [DataSpec, DataSpecJSONEncoder, DataSpecJSONDecoder]
+__all__ = ["DataSpec", "DataSpecJSONEncoder", "DataSpecJSONDecoder"]
